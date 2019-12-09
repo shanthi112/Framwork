88d3bf0f4f3aa873971a6e46d4b17f02eb33ae54
test <test@10.10.15.113>
2019-12-09 15:32:48 +0530

    push code 2

---
+++ Example/src/SeleniumPractice.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "Downloads//chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("");
+		
+		
+	}
+
+}
